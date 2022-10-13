d4d1812da9cad7ab88d961b8cc0688e084ec9812
Olami1998 <abisolaolalekan19@gmail.com>
2022-10-13 13:51:54 -0700

    ~

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,11 +7,9 @@
  *		a piece of art," followed by a new line.
  *
  * Return: Always 0 (Success)
- *
  */
 
 int main(void)
-
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 
