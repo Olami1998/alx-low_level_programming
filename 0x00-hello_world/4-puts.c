cc35bbb9b0c4d5c9e61c9ca49168d80c017bfbbb
Olami1998 <abisolaolalekan19@gmail.com>
2022-10-13 13:38:08 -0700

    ~

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,13 +6,13 @@
 
 /**
  * main - Prints "Programming is like building a multilingual
- * 		puzzle, followed by a new line.
+ *		puzzle, followed by a new line.
  *
- * 		Return: Always 0.
+ *		Return: Always 0.
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzel");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
