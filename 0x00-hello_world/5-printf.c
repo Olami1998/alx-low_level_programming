18975f3c99a5dd9ce9628178173c2aa06986a57b
Olami1998 <abisolaolalekan19@gmail.com>
2022-10-14 05:28:36 -0700

    ~

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -14,13 +14,10 @@
  *
  * main - Prints "with proper grammar, but the outcome is a piece of art," followed by a new line.
  *
- * 
- *
  * Return: Always 0.
  */
 
 int main(void)
-
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
