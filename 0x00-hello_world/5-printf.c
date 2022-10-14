5d0a2745ada7c653248cbf9d6a3a5160e887da9c
Olami1998 <abisolaolalekan19@gmail.com>
2022-10-14 05:20:02 -0700

    ~

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -12,7 +12,7 @@
 
 /**
  *
- *  * main - Prints "with proper grammar, but the outcome is
+ * main - Prints "with proper grammar, but the outcome is
  *
  *                a piece of art," followed by a new line.
  *
