d008c75b3ddb8f4d52486347cf9bbfa80e371bf8
Olami1998 <abisolaolalekan19@gmail.com>
2022-10-18 14:40:29 -0700

    ~

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add two munbers
+ * @a: the first number
+ * @b: the second nuymber
+ * Return: the addition of both
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
