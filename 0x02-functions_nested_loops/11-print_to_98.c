953cd5d6281d429cbcfe51f08bb4845e648a7167
Olami1998 <abisolaolalekan19@gmail.com>
2022-10-19 07:17:44 -0700

    ~

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
  * print_to_98 - print all numbers from input to 98, in order separated by a
- * 		comma followed by a space
+ *		comma followed by a space
  * @n: the starting number
  */
 void print_to_98(int n)
