9a1f0369075f32a1ea53e6909fb0baa9af47dd7c
Olami1998 <abisolaolalekan19@gmail.com>
2022-10-21 14:42:26 -0700

    ~

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#indef MAIN_H
-#define MAIN_H
-
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -13,5 +10,3 @@ void print_square(int size);
 void print_triangle(int size);
 int _putchar(char c);
 void print_number(int n);
-
-#endif
