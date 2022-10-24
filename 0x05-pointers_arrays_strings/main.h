1231239f52930190282d8f996f8bdb5ea5113136
Olami1998 <abisolaolalekan19@gmail.com>
2022-10-24 14:09:07 -0700

    ~

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,3 @@
-#ifndef MAIN_H_
-#define MAIN_H_
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -13,4 +11,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *scr);
 int _putchar(char c);
 
-#endif
+
